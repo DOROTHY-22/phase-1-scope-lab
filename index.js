783b03d38ba1ede5fe77b7be8f97d778e134ea92
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-03-07 13:44:35 +0300

    Fniish my assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+var customerName = 'bob';
+function upperCaseCustomerName(){
+ customerName = (customerName.toUpperCase())
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'Walter';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer ='Albert';
+
+}
+
+
+
+
+
 // Write your solution in this file!
